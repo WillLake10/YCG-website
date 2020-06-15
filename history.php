bbbde575a635dbeb47de763fb80068a65747a79a
BuildTools <unconfigured@null.spigotmc.org>
2020-06-15 22:18:27 +0100

    update to php

---
+++ history.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<!--
+Website: York Colleges Guild
+Type: HTML5
+Page: Peals and Quarters
+Created: 28th April 2020
+Last modified: 28th April 2020
+Author: Will Lake
+-->
+<html lang="en">
+<head>
+    <script src="scripts/head.js" type="text/javascript"></script>
+    <script src="scripts/divCalls.js" type="text/javascript"></script>
+</head>
+<body>
+
+<div id="pageTop"></div>
+
+<div id="splitterFull"></div>
+
+<nav class="navbar navbar-expand-lg bg-light navbar-light sticky-top">
+    <div id="navbarOuter"></div>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <div id="navbarItems"></div>
+    </div>
+</nav>
+
+<script src="scripts/backToTopMove.js" type="text/javascript"></script>
+
+<section id="pagetitle">
+    <div class="container pt-3 pb-3">
+        <div class="row">
+            <div class="col-md-3"></div>
+            <div class="col-12 col-md-6">
+                <div class="pagetitle">
+                    History
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="recent">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <p>The development of a Students' Bellringing Society in York can be traced back to the expansion of
+                    Higher Education in the 1950's.</p>
+
+                <p>The first organised student ringing took place late in 1962 with the formation of a bellringing
+                    society at St John's College. The initiators of this group were Michael King from Wentworth, South
+                    Yorkshire, and Ray Aldington from Kings Norton, Birmingham. They organised ringing on Wednesday
+                    evenings at the former church of St John in Micklegate, which at that time was The Institute of
+                    Advanced Architectural Studies. It subsequently became York Arts Centre, and is currently
+                    disused.</p>
+
+                <p>The bells at St John's were an ancient ring of six, but had been rehung in a new bellframe and on new
+                    fittings in 1954.</p>
+
+                <p>The St John's College Bellringers' Society continued to expand during the 1960's and held a full
+                    programme of ringing events, including an annual tour. They also organised handbell ringing and
+                    purchased a secondhand set of 24 handbells.</p>
+            </div>
+            <div class="col-md-6 pb-3">
+                <img src="images/history/st_john.jpg" class="img-fluid mx-auto d-block">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <p>The University of York grew slowly in various historic buildings in the city centre during the
+                    1950's. The first undergraduates were accepted in 1963 and the Heslington campus was ready for
+                    occupation in October 1965. In that year, Derek Jones from Spalding, came as an undergraduate.
+                    Together with another ringer, Christina Folkes from St Albans, he took the first tentative steps
+                    towards forming The University of York Society of Bellringers. The earliest activity of this group
+                    can be traced to October 1967.</p>
+
+                <p>In September 1969, David Potter came to St John's College. He restarted ringing at the former church
+                    of St John, where ringing had ceased. The belltower had become unsafe after the demolition of the
+                    adjoining building and required remedial strengthening. This had been completed in 1968.</p>
+
+                <p>By this date, both St John's Society and the University Society were becoming weak. David Potter met
+                    with Derek Jones to discuss joining student bellringers into one unified group. The idea seemed a
+                    logical step since neither group was large enough to definitely survive if it remained
+                    independent.</p>
+
+                <p>The decision to form The York Colleges Guild of Bell Ringers was taken in November 1969 and its first
+                    meeting took place at the former church of St John in January 1970.</p>
+
+                <p>The York Colleges Guild has operated continuously since its foundation, operating a full programme of
+                    ringing and social events throughout each academic year.</p>
+
+                <p>Although bellringers have been mainly recruited from The University of York and York St John, other
+                    members have come from Askham Bryan Agricultural College, York College of Arts & Technology, The Law
+                    College and York Sixth Form College.</p>
+
+                <p>The York Colleges Guild of Bell Ringers is a flourishing organisation. It now rings at various towers
+                    in the city centre, most usually The Spurriergate Centre (formerly St Michael's Church,
+                    Spurriergate) and St Lawrence's Church</p>
+            </div>
+        </div>
+    </div>
+</section>
+<br><br>
+</body>
+</html>
---
+++ pealsQuarters.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<!--
+Website: York Colleges Guild
+Type: HTML5
+Page: Peals and Quarters
+Created: 20th April 2020
+Last modified: 14th June 2020
+Author: Will Lake
+-->
+<html lang="en">
+<head>
+    <script src="scripts/head.js" type="text/javascript"></script>
+    <script src="scripts/divCalls.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="styles/peal.css" type="text/css">
+</head>
+<body>
+
+<section id="top"></section>
+
+<div id="pageTop"></div>
+
+<div id="splitterFull"></div>
+
+<nav class="navbar navbar-expand-lg bg-light navbar-light sticky-top">
+    <div id="navbarOuter"></div>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <div id="navbarItems"></div>
+    </div>
+</nav>
+
+<script src="scripts/backToTopMove.js" type="text/javascript"></script>
+
+<section id="pagetitle">
+    <div class="container pt-3 pb-3">
+        <div class="row">
+            <div class="col-md-3"></div>
+            <div class="col-12 col-md-6">
+                <div class="pagetitle">
+                    Peals and Quarters
+                </div>
+            </div>
+        </div>
+        <div class="row pt-3">
+            <p>This is a record of the peals and quarter peals rung by members of the York Colleges Guild. This list is
+                by
+                no means complete, but we try to keep it as up to date as possible.</p>
+            <p>Records are displayed in academic year from September 1st to August 31st</p>
+        </div>
+    </div>
+</section>
+
+<script src="scripts/pealNav.js" type="text/javascript"></script>
+
+<script src="scripts/pealsRecent.js" type="text/javascript"></script>
+
+<script src="scripts/peals.js" type="text/javascript"></script>
+
+<script src="scripts/backToTop.js" type="text/javascript"></script>
+
+</body>
+</html>

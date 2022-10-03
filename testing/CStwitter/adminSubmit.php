09e7be1b21c9459cce4ee87205e130eef51dc030
WillLake10 <william.j.lake2000@gmail.com>
2022-10-03 23:13:50 +0100

    Change permissions

--- testing/CStwitter/adminSubmit.php
+++ testing/CStwitter/adminSubmit.php
@@ -24,16 +24,18 @@
 
             $allTweetAdmin = array();
             foreach ($tweets as $tweet) {
-                $tweetAd = new TweetAdmin();
-                $tweetAd->setId($tweet->id);
+                echo "<p>loop start</p>";
+
+//                $tweetAd = new TweetAdmin();
+//                $tweetAd->setId($tweet->id);
                 if ($_POST["$tweet->id-hide"] == "hidden"){
-                    $tweetAd->setHideFromCarousel("true");
+//                    $tweetAd->setHideFromCarousel("true");
                     echo "<p>Tweet $tweet->id is hidden from the carousel</p>";
                 }
-                else {
-                    $tweetAd->setHideFromCarousel("false");
-                }
-                $allTweetAdmin[] = $tweetAd;
+//                else {
+//                    $tweetAd->setHideFromCarousel("false");
+//                }
+//                $allTweetAdmin[] = $tweetAd;
             }
 
             $tweetAdminFile = fopen("tweetsAdmin.json", "w") or die("Unable to open file!");
@@ -45,24 +47,5 @@
         <input type="submit" value="Back To Admin" />
     </form>
 </div>
-<script>
-    let slideIndex = 0;
-    showSlides();
-
-    function showSlides() {
-        let i;
-        let slides = document.getElementsByClassName("mySlides");
-        // let dots = document.getElementsByClassName("dot");
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-        slideIndex++;
-        if (slideIndex > slides.length) {
-            slideIndex = 1
-        }
-        slides[slideIndex - 1].style.display = "block";
-        setTimeout(showSlides, 5000); // Change image every 2 seconds
-    }
-</script>
 </body>
 

6c181340ead5e218b024d29ee4ad7558354c404a
WillLake10 <william.j.lake2000@gmail.com>
2022-10-17 18:44:04 +0100

    Peals summary change

--- pealsSummary.php
+++ pealsSummary.php
@@ -33,63 +33,157 @@ Author: Will Lake
     </div>
 </section>
 
-<section id="pagetitle">
+<section id="test">
     <div class="container pt-3 pl-2 pr-2">
         <div class="row">
-                <table class="table table-striped table-bordered text-center">
-                    <thead>
-                    <tr>
-                        <th scope="col" style="width: 20%">Year</th>
-                        <th scope="col" style="width: 20%">Peals</th>
-                        <th scope="col" style="width: 20%">Quarters</th>
-                        <th scope="col" style="width: 20%">Other</th>
-                        <th scope="col" style="width: 20%">Total</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <?php
-                    $url = 'peals/counts.json';
-                    $counts = file_get_contents($url);
-                    $counts = json_decode($counts);
+            <div class="grid-container">
+                <div class="year">
+                    Year
+                </div>
+                <div class="peals bold">
+                    Peals
+                </div>
+                <div class="quarters bold">
+                    Quarters
+                </div>
+                <div class="other bold">
+                    Other
+                </div>
+                <div class="total bold">
+                    Total
+                </div>
+            </div>
+            <?php
+            $url = 'peals/counts.json';
+            $counts = file_get_contents($url);
+            $counts = json_decode($counts);
 
-                    foreach ($counts as $element) {
-                        $total = $element->peals+$element->quarters+$element->other;
-                        echo "<tr>";
-                        echo "<th scope=\"row\">$element->academic_year</th>";
-                        echo "<td>$element->peals</td>";
-                        echo "<td>$element->quarters</td>";
-                        echo "<td>$element->other</td>";
-                        echo "<td>$total</td>";
-                        echo "</tr>";
-                        if ($element->academic_year === "2021/22") {
-                            echo "<tr>";
-                            echo "<th scope=\"row\">2020/21</th>";
-                            echo "<td>0</td>";
-                            echo "<td>0</td>";
-                            echo "<td>0</td>";
-                            echo "<td>0</td>";
-                            echo "</tr>";
-                        }
-                        if ($element->academic_year === "2004/05") {
-                            echo "<tr>";
-                            echo "<th scope=\"row\">2003/04</th>";
-                            echo "<td>0</td>";
-                            echo "<td>0</td>";
-                            echo "<td>0</td>";
-                            echo "<td>0</td>";
-                            echo "</tr>";
+            $url_loc = 'peals/locationsCounts.json';
+            $locations = file_get_contents($url_loc);
+            $locations = json_decode($locations);
+
+            foreach ($counts as $element) {
+                $total = $element->peals + $element->quarters + $element->other;
+                echo "<div class=\"grid-container\">";
+                echo "<div class=\"year\">$element->academic_year</div>";
+                echo "<div class=\"peals\">$element->peals</div>";
+                echo "<div class=\"quarters\">$element->quarters</div>";
+                echo "<div class=\"other\">$element->other</div>";
+                echo "<div class=\"total\">$total</div>";
+                echo "<div class=\"expand\">";
+                echo "<button class=\"collapsible\" id=\"button-$element->academic_year\" onclick=\"myFunction('$element->academic_year')\"></button>";
+                echo "</div>";
+                echo "<div class=\"extra\" id=\"content-$element->academic_year\" style=\"display: none\">";
+                foreach ($locations as $years) {
+                    if ($years->academic_year === $element->academic_year){
+                        foreach ($years->tower as $tower){
+                            echo "<div class=\"grid-container-location\">";
+                            echo "<div class=\"tower\">$tower->tower</div>";
+                            echo "<div class=\"peals\">$tower->peals</div>";
+                            echo "<div class=\"quarters\">$tower->quarters</div>";
+                            echo "<div class=\"other\">$tower->other</div>";
+                            echo "<div class=\"total\">$tower->total</div>";
+                            echo "</div>";
                         }
                     }
-                    ?>
-                    </tbody>
-                </table>
+                }
+
+
+
+                echo "</div>";
+                echo "</div>";
+                if ($element->academic_year === "2021/22") {
+                    echo "<div class=\"grid-container\">";
+                    echo "<div class=\"year\">2020/21</div>";
+                    echo "<div class=\"peals\">0</div>";
+                    echo "<div class=\"quarters\">0</div>";
+                    echo "<div class=\"other\">0</div>";
+                    echo "<div class=\"total\">0</div>";
+//                    echo "<div class=\"expand\"></div>";
+//                    echo "<div class=\"extra\" style=\"display: none\"></div>";
+                    echo "</div>";
+                }
+                if ($element->academic_year === "2004/05") {
+                    echo "<div class=\"grid-container\">";
+                    echo "<div class=\"year\">2003/04</div>";
+                    echo "<div class=\"peals\">0</div>";
+                    echo "<div class=\"quarters\">0</div>";
+                    echo "<div class=\"other\">0</div>";
+                    echo "<div class=\"total\">0</div>";
+//                    echo "<div class=\"expand\"></div>";
+//                    echo "<div class=\"extra\" style=\"display: none\"></div>";
+                    echo "</div>";
+                }
+            }
+            ?>
         </div>
     </div>
 </section>
 
-<section>
+<section id="pagetitle">
+    <div class="container pt-3 pl-2 pr-2">
+        <div class="row">
+            <table class="table table-striped table-bordered text-center">
+                <thead>
+                <tr>
+                    <th scope="col" style="width: 20%">Year</th>
+                    <th scope="col" style="width: 20%">Peals</th>
+                    <th scope="col" style="width: 20%">Quarters</th>
+                    <th scope="col" style="width: 20%">Other</th>
+                    <th scope="col" style="width: 20%">Total</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                $url = 'peals/counts.json';
+                $counts = file_get_contents($url);
+                $counts = json_decode($counts);
 
+                foreach ($counts as $element) {
+                    $total = $element->peals + $element->quarters + $element->other;
+                    echo "<tr>";
+                    echo "<th scope=\"row\">$element->academic_year</th>";
+                    echo "<td>$element->peals</td>";
+                    echo "<td>$element->quarters</td>";
+                    echo "<td>$element->other</td>";
+                    echo "<td>$total</td>";
+                    echo "</tr>";
+                    if ($element->academic_year === "2021/22") {
+                        echo "<tr>";
+                        echo "<th scope=\"row\">2020/21</th>";
+                        echo "<td>0</td>";
+                        echo "<td>0</td>";
+                        echo "<td>0</td>";
+                        echo "<td>0</td>";
+                        echo "</tr>";
+                    }
+                    if ($element->academic_year === "2004/05") {
+                        echo "<tr>";
+                        echo "<th scope=\"row\">2003/04</th>";
+                        echo "<td>0</td>";
+                        echo "<td>0</td>";
+                        echo "<td>0</td>";
+                        echo "<td>0</td>";
+                        echo "</tr>";
+                    }
+                }
+                ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </section>
+
+<script>
+    function myFunction(year) {
+        var x = document.getElementById("content-" + year);
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+</script>
 
 <br><br>
 </body>

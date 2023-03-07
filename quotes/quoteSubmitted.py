5481a63b366308f503f1674e87661905f4d9da0c
Will Lake <william.j.lake2000@gmail.com>
2023-03-07 15:06:10 +0000

    testing

--- quotes/quoteSubmitted.py
+++ quotes/quoteSubmitted.py
@@ -1,9 +1,9 @@
 import os
 
-# input_variable = os.environ['GITHUB_ENV']
-
 print(os.environ)
-# print("Input Variable:", input_variable)
+
+input_variable = os.environ['INPUT_STORE']
+print("Input Variable:", input_variable)
 #
 # f = open(input_variable, "r")
 # print(f.read())

d7b56dac544e56aca85e037bea254fd12189a7b0
Will Lake <william.j.lake2000@gmail.com>
2023-03-07 15:01:48 +0000

    Fix names

--- quotes/quoteSubmitted.py
+++ quotes/quoteSubmitted.py
@@ -1,8 +1,9 @@
 import os
 
-input_variable = os.environ['GITHUB_ENV']
+# input_variable = os.environ['GITHUB_ENV']
 
-print("Input Variable:", input_variable)
-
-f = open(input_variable, "r")
-print(f.read())
+print(os.environ)
+# print("Input Variable:", input_variable)
+#
+# f = open(input_variable, "r")
+# print(f.read())

d32725d338e99030c7ae680266db3d2b48d395d0
GitHub Action <action@github.com>
2023-03-08 21:46:07 +0000

    New Quote Submitted

---
+++ quotes/pending/4368802464-1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Quotes - York Colleges Guild</title>
+    <link rel="stylesheet" href="styles/quotes.css" type="text/css">
+</head>
+<body>
+<h1>New Quote for quote page</h1>
+<p>Click Approve To add </p>
+<p>Quote is:</p><p>James D Austin: I'm consistently shit<br>: <br>: <br>: </p><br><p> Submitted by Nat</p><form action="../quoteApproved.php" method="post"><input id="quoteId" name="quoteId" value="4368802464-1" /><input type="submit" value="Approve" /></form><form action="../quoteDisapproved.php" method="post"><input id="quoteId" name="quoteId" value="4368802464-1" /><input type="submit" value="Disapprove" /></form>

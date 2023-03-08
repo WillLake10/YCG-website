452e17100c40d51104de5a5855952da5256cdc25
Will Lake <william.j.lake2000@gmail.com>
2023-03-08 11:31:32 +0000

    Update sitemap

--- quotes/quoteSubmitted.py
+++ quotes/quoteSubmitted.py
@@ -1,5 +1,20 @@
 import os
+import json
 
-input_variable = os.environ['INPUT_STORE']
-print("Input Variable:", input_variable)
+input_variable = {
+    "username": os.environ['INPUT_USERNAME'],
+    "email": os.environ['INPUT_EMAIL'],
+    "code": os.environ['INPUT_CODE'],
+    "quote": {
+        "name1": os.environ['INPUT_CODE'],
+        "quote1": os.environ['INPUT_CODE'],
+        "name2": os.environ['INPUT_CODE'],
+        "quote2": os.environ['INPUT_CODE'],
+        "name3": os.environ['INPUT_CODE'],
+        "quote3": os.environ['INPUT_CODE'],
+        "name4": os.environ['INPUT_CODE'],
+        "quote4": os.environ['INPUT_CODE']
+    }
+}
 
+print("Input Variable:", json.dumps(input_variable))

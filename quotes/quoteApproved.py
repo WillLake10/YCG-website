a0d7cd088dfbd4a142145631e0e492145dd9bae6
Will Lake <william.j.lake2000@gmail.com>
2023-03-08 18:55:34 +0000

    Adjust

--- quotes/quoteApproved.py
+++ quotes/quoteApproved.py
@@ -4,5 +4,5 @@ quote_id = os.environ['INPUT_QUOTE_ID']
 
 print(quote_id)
 
-os.remove("quotes/" + quote_id + ".php")
-os.remove("quotes/" + quote_id + ".json")
+os.remove("quotes/pending/" + quote_id + ".php")
+os.remove("quotes/pending/" + quote_id + ".json")

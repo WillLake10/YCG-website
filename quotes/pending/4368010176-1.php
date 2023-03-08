3d49b1cf75a0a332fe55a3c2defbcb267ef3d5af
GitHub Action <action@github.com>
2023-03-08 19:57:12 +0000

    New Quote Submitted

---
+++ quotes/pending/4368010176-1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Quotes - York Colleges Guild</title>
+    <link rel="stylesheet" href="styles/quotes.css" type="text/css">
+</head>
+<body>
+<h1>New Quote for quote page</h1>
+<p>Click Approve To add </p>
+<p>Quote is:</p><p>Will: Im a train<br>df: d<br>da: d<br>dasz: ad</p><br><p> Submitted by Will Lake</p><form action="../quoteApproved.php" method="post"><input id="quoteId" name="quoteId" value="4368010176-1" /><input type="submit" value="Approve" /></form>

e71008967a345862eebfa46b899115ef2c92423b
GitHub Action <action@github.com>
2023-03-08 18:36:28 +0000

    New Quote Submitted

---
+++ quotes/4367420908-1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Quotes - York Colleges Guild</title>
+    <link rel="stylesheet" href="styles/quotes.css" type="text/css">
+</head>
+<body>
+<h1>New Quote for quote page</h1>
+<p>Click Approve To add </p>
+<p>Quote is:</p><p>Will L: poo<br>: <br>: <br>: </p><br><p> Submitted by WillJLake123</p><form action="quoteApproved.php" method="post"><input id="quoteId" name="quoteId" value="4367420908-1" /><input type="submit" value="Approve" /></form>

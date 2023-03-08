77f971a2a95782a187121b4de5c8c6c9175df78e
GitHub Action <action@github.com>
2023-03-08 20:53:37 +0000

    New Quote Submitted

---
+++ quotes/pending/4368429354-1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Quotes - York Colleges Guild</title>
+    <link rel="stylesheet" href="styles/quotes.css" type="text/css">
+</head>
+<body>
+<h1>New Quote for quote page</h1>
+<p>Click Approve To add </p>
+<p>Quote is:</p><p>Matthew: I'm pro women, as a concept<br>: <br>: <br>: </p><br><p> Submitted by Nat</p><form action="../quoteApproved.php" method="post"><input id="quoteId" name="quoteId" value="4368429354-1" /><input type="submit" value="Approve" /></form><form action="../quoteDisapproved.php" method="post"><input id="quoteId" name="quoteId" value="4368429354-1" /><input type="submit" value="Disapprove" /></form>

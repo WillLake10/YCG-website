c99d97c70dcb87621a8d5347df05deb8f57780ce
GitHub Action <action@github.com>
2023-03-09 15:41:41 +0000

    New Quote Submitted

---
+++ quotes/pending/4376169595-1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Quotes - York Colleges Guild</title>
+    <link rel="stylesheet" href="styles/quotes.css" type="text/css">
+</head>
+<body>
+<h1>New Quote for quote page</h1>
+<p>Click Approve To add </p>
+<p>Quote is:</p><p>Ben: That doesn't look like Stedman?<br>William M Regan !bracketO!Friend!bracketC!: That's because it's Yorkshire, Ben<br>: <br>: </p><br><p> Submitted by Nat</p><form action="../quoteApproved.php" method="post"><input id="quoteId" name="quoteId" value="4376169595-1" /><input type="submit" value="Approve" /></form><form action="../quoteDisapproved.php" method="post"><input id="quoteId" name="quoteId" value="4376169595-1" /><input type="submit" value="Disapprove" /></form>

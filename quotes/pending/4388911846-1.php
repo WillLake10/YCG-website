dc08945aa27c589aec9a0e32d3cf3663cc3cd156
GitHub Action <action@github.com>
2023-03-10 22:20:13 +0000

    New Quote Submitted

---
+++ quotes/pending/4388911846-1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Quotes - York Colleges Guild</title>
+    <link rel="stylesheet" href="styles/quotes.css" type="text/css">
+</head>
+<body>
+<h1>New Quote for quote page</h1>
+<p>Click Approve To add </p>
+<p>Quote is:</p><p>Ben: I wouldn't put it past russian dictators to like, when they're embalmed, get surgeries to make theyr componants event.css images peals quotes scripts styles bigger<br>: <br>: <br>: </p><br><p> Submitted by Nat</p><form action="../quoteApproved.php" method="post"><input id="quoteId" name="quoteId" value="4388911846-1" /><input type="submit" value="Approve" /></form><form action="../quoteDisapproved.php" method="post"><input id="quoteId" name="quoteId" value="4388911846-1" /><input type="submit" value="Disapprove" /></form>

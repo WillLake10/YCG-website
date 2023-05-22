2c4fe7e07fc8980312d1a52b0d4dc370f7d3b1bd
GitHub Action <action@github.com>
2023-05-22 20:47:41 +0000

    New Quote Submitted

---
+++ quotes/pending/5050263531-1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Quotes - York Colleges Guild</title>
+    <link rel="stylesheet" href="styles/quotes.css" type="text/css">
+</head>
+<body>
+<h1>New Quote for quote page</h1>
+<p>Click Approve To add </p>
+<p>Quote is:</p><p>Caitlin: I've got some pumice... I like the way it floats!<br>: <br>: <br>: </p><br><p> Submitted by mima</p><form action="../quoteApproved.php" method="post"><input id="quoteId" name="quoteId" value="5050263531-1" /><input type="submit" value="Approve" /></form><form action="../quoteDisapproved.php" method="post"><input id="quoteId" name="quoteId" value="5050263531-1" /><input type="submit" value="Disapprove" /></form>

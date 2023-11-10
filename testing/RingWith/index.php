707018e8b63867ca2c93eddbd5dad836290ed6b5
WillLake10 <william.j.lake2000@gmail.com>
2023-11-10 19:31:04 +0000

    Change to php

--- testing/RingWith/index.php
+++ testing/RingWith/index.php
@@ -6,5 +6,23 @@
 </head>
 <body>
 <h1>Who have I rung with</h1>
+<input id="numb">
+<button type="button" onclick="myFunction()">Submit</button>
+<p id="demo"></p>
 </body>
+<script>
+    function myFunction() {
+        fetch('https://bb.ringingworld.co.uk/search.php?place=york&pagesize=20000', {
+            method: 'GET',
+            headers: {
+                'Access-Control-Allow-Origin': 'https://www.ycg.org.uk'
+            },
+        })
+            .then(response => text = response)
+            .then(response => console.log(response))
+            .catch(err => console.error(err));
+
+        document.getElementById("demo").innerHTML = text;
+    }
+</script>
 </html>
